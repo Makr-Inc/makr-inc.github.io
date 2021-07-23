b66598bf9abbe88579b2cb9da75c7bc818e605f5
Mike Ulvila <mike.ulvila@quore.com>
2021-07-23 14:12:24 -0500

    remove capital letters from repo name to work with github pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Makr-Inc.github.io',
+    title: 'makr-inc.github.io',
     htmlAttrs: {
       lang: 'en'
     },
