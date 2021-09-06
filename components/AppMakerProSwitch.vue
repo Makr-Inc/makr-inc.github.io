75f027f8554835fe02191f06367941426d2a0f7e
Mike Ulvila <mike.ulvila@quore.com>
2021-09-06 08:42:46 -0500

    make app maker pro switcher

---
+++ components/AppMakerProSwitch.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <b-field class="is-inline-block p-2 has-background-primary">
+      <b-radio-button v-model="switchValue"
+                      native-value="maker"
+                      type="is-primary">
+        Makers
+      </b-radio-button>
+
+      <b-radio-button v-model="switchValue"
+                      native-value="pro"
+                      type="is-primary">
+        Pros
+      </b-radio-button>
+    </b-field>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      switchValue: ''
+    }
+  },
+  watch: {
+    switchValue(newValue) {
+      this.$emit('switch-value', newValue)
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
+    <app-maker-pro-switch @switch-value="handleSwitchValue"></app-maker-pro-switch>
     <app-hero :title="hero.title" :subtitle="hero.subtitle"></app-hero>
   </div>
 </template>
 
 <script>
 import AppHero from '~/components/AppHero'
+import AppMakerProSwitch from '~/components/AppMakerProSwitch'
+
 export default {
   components: {
-    AppHero
+    AppHero,
+    AppMakerProSwitch
   },
   data() {
     return {
@@ -16,6 +20,11 @@ export default {
         title: 'Get paid for your personalized expertise from anywhere, anytime.',
         subtitle: 'Help makers accomplish more specific problems with their projects, right from your device'
       }
+    }
+  },
+  methods: {
+    handleSwitchValue(value) {
+      console.log('emitted value:: ', value)
     }
   }
 }
