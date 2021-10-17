dca3e93bebebdc83fccf360a3a2da0c66d0ddccf
mikeulvila <mikeulvila@users.noreply.github.com>
2021-10-17 01:43:19 +0000

    deploy: 062ff6fd77c728db32f2cd5977a79e2f62ff77fc

---
+++ _nuxt/static/1634434996/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634434996",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"304a8f3b"}}}}(null,"\u002F"));
