062ff6fd77c728db32f2cd5977a79e2f62ff77fc
Mike Ulvila <mike.ulvila@quore.com>
2021-10-16 20:41:08 -0500

    more pro content

--- components/AppHowItWorks.vue
+++ components/AppHowItWorks.vue
@@ -8,51 +8,69 @@
       </div>
     </div>
     <div class='columns'>
-      <div class='column is-flex is-flex-grow-1'>
+      <div v-for='item in howItWorks' :key='item.title' class='column is-flex is-flex-grow-1'>
         <div class='card'>
           <div class='card-content'>
             <b-icon
-              icon="lightning-bolt"
+              :icon="item.icon"
               size="is-medium"
               type="is-black">
             </b-icon>
-            <p class='has-text-weight-bold'>1. Get matched with a pro</p>
+            <p class='has-text-weight-bold'>{{ item.title }}</p>
             <br>
-            <p>We can help with any project. Find a pro that works for your schedule and budget.</p>
-          </div>
-        </div>
-      </div>
-      <div class='column is-flex is-flex-grow-1'>
-        <div class='card'>
-          <div class='card-content'>
-            <b-icon
-              icon="comment-text"
-              size="is-medium"
-              type="is-black">
-            </b-icon>
-            <p class='has-text-weight-bold'>2. Share your project details</p>
-            <br>
-            <p>Let your pro see your project’s specific challenges so they can effectively guide you to a solution.</p>
-          </div>
-        </div>
-      </div>
-      <div class='column is-flex is-flex-grow-1'>
-        <div class='card'>
-          <div class='card-content'>
-            <b-icon
-              icon="tooltip-account"
-              size="is-medium"
-              type="is-black">
-            </b-icon>
-            <p class='has-text-weight-bold'>3. Receive personalized guidance to achieve your project’s goals</p>
-            <br>
-            <p>Get the feedback and confidence you need on your project all from interactive video or real-time chat with your pro.</p>
+            <p>{{ item.description }}</p>
           </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    isMaker: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data() {
+    return {
+      howItWorksMaker: [{
+        icon: 'lightning-bolt',
+        title: '1. Get matched with a pro',
+        description: 'We can help with any project. Find a pro that works for your schedule and budget.'
+      }, {
+        icon: 'comment-text',
+        title: '2. Share your project details',
+        description: 'Let your pro see your project’s specific challenges so they can effectively guide you to a solution.'
+      }, {
+        icon: 'tooltip-account',
+        title: '3. Receive personalized guidance to achieve your project’s goals',
+        description: 'Get the feedback and confidence you need on your project all from interactive video or real-time chat with your pro.'
+      }],
+      howItWorksPro: [{
+        icon: 'wrench',
+        title: '1. Tell us your skill set',
+        description: 'Sign up to share your expertise with our makers and their projects. Set your own rate and availability.'
+      }, {
+        icon: 'cellphone-wireless',
+        title: '2. Share your expertise',
+        description: 'Based on your skills, makers will book video sessions on your schedule to get your expert guidance.'
+      }, {
+        icon: 'currency-usd',
+        title: '3. Earn cash from anywhere',
+        description: 'Get paid for helping makers accomplish more with their projects!'
+      }]
+    }
+  },
+  computed: {
+    howItWorks() {
+      return this.isMaker ? this.howItWorksMaker : this.howItWorksPro
+    }
+  }
+}
+</script>
 
 <style lang='scss' scoped>
 .how-it-works {
--- pages/index.vue
+++ pages/index.vue
@@ -22,7 +22,7 @@
         <app-explore-projects :explore-project-cards='exploreProjectCards'></app-explore-projects>
       </div>
     </div>
-    <div class='columns is-centered'>
+    <div class='columns is-centered mb-6'>
       <div class='column is-three-fifths'>
         <h3 class='mb-3 is-size-3 has-text-weight-bold has-text-centered'>Meet Makr</h3>
         <p class='px-6 mb-2 has-text-centered'>Welcome to Makr, the platform that helps the makers, the doers, and the
@@ -35,7 +35,7 @@
         </figure>
       </div>
     </div>
-    <div class='columns'>
+    <div v-if='isMaker' class='columns'>
       <div class='column'>
         <h3 class='is-size-4 has-text-weight-bold has-text-centered'>
           Every project has its unique challenges.<br>
@@ -43,11 +43,11 @@
         </h3>
       </div>
     </div>
-    <div class="carousel-container">
+    <div v-if='isMaker' class="carousel-container">
       <app-testimonial-carousel></app-testimonial-carousel>
     </div>
     <!--    get feedback fast block -->
-    <div class="columns is-vcentered">
+    <div v-if='isMaker' class="columns is-vcentered">
       <div class="column"></div>
       <div class="column is-one-third">
         <img class="image" :src="require('~/assets/get-feedback.png')" alt="get-feedback-image"/>
@@ -63,10 +63,12 @@
       </div>
       <div class="column"></div>
     </div>
-    <!--    how it works block-->
-    <app-how-it-works class='mb-6'></app-how-it-works>
-    <!--    become a makr pro-->
-    <div class='become-a-pro columns'>
+
+    <!--    how it works maker block-->
+    <app-how-it-works v-if='isMaker' :is-maker='isMaker' class='mb-6'></app-how-it-works>
+
+<!--    become a makr pro-->
+    <div v-if='isMaker' class='become-a-pro columns mb-6'>
       <div class='column is-two-thirds'>
         <p class="title is-spaced">
           Become a Makr pro
@@ -77,9 +79,61 @@
         <b-button type="is-primary">Learn More</b-button>
       </div>
       <div class="column is-one-third is-flex level">
-        <img class="level-item" :src="require('~/assets/become-a-makr.png')" alt="hero-image"/>
+        <img class="level-item" :src="require('~/assets/become-a-makr.png')" alt="become a pro"/>
       </div>
     </div>
+
+<!--    set your own hours pro block-->
+    <div v-if='!isMaker' class='columns is-vcentered mb-6'>
+      <div class='column is-4 is-offset-2'>
+        <img class="level-item" :src="require('~/assets/become-a-makr.png')" alt="become a pro"/>
+      </div>
+      <div class='column is-4'>
+        <p class="title is-spaced">
+          Set your own hours.
+          <br>
+          Earn on your own terms.
+        </p>
+        <div class='mb-4'>
+          <p class='has-text-weight-bold'>Earn anytime</p>
+          <p>You can work when you want and earn money instantly</p>
+        </div>
+        <div class='mb-4'>
+          <p class='has-text-weight-bold'>Set your own schedule</p>
+          <p>Only work when it’s convenient for you. You own your schedule</p>
+        </div>
+        <div class='mb-4'>
+          <p class='has-text-weight-bold'>Help makers from anywhere</p>
+          <p>Share your expertise with makers from anywhere in the world. All you need is a connection to earn</p>
+        </div>
+      </div>
+    </div>
+
+<!--    full liability coverage block-->
+    <div v-if='!isMaker' class='columns is-centered mb-6'>
+      <div class='column is-8'>
+        <div class='notification is-warning is-light has-text-centered px-6 has-text-black'>
+          <p class='has-text-weight-bold is-size-5'>
+            Full liability coverage
+          </p>
+          <p>We’ve got you covered so you can help makers with ease. We ensure that you’re never liable for projects you’re supporting through Makr.</p>
+        </div>
+      </div>
+    </div>
+
+<!--    pro how it works block-->
+    <app-how-it-works v-if='!isMaker' :is-maker='isMaker' class='mb-6'></app-how-it-works>
+
+<!--    opportunity is waiting for you-->
+    <div class='columns is-centered'>
+      <div class='column has-text-centered p-5 mb-6 has-background-primary-light'>
+        <p class='title is-size-5 mb-3'>Opportunity is waiting for you</p>
+        <p class='mb-3'>Join today to support projects and earn on your own terms</p>
+        <b-button type='is-primary'>Become a Makr Pro</b-button>
+      </div>
+    </div>
+
+<!--    common questions block-->
     <div class='columns is-centered'>
       <div class='column is-two-thirds'>
         <p class='title has-text-centered is-size-4'>Common Questions</p>
