021844e17350cf18e0b1c7c89f4b0e8ce2da67e5
Mike Ulvila <mike.ulvila@quore.com>
2021-11-17 08:35:31 -0600

    change head title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'makr-inc.github.io',
+    title: 'Makr',
     htmlAttrs: {
       lang: 'en'
     },
